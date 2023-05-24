65aeb198e7207fc426b41e7e4f5295bd36c90f4e
benpankow <ben@elementl.com>
2023-05-24 17:53:40 +0400

    fix more tests

--- python_modules/dagster-graphql/dagster_graphql_tests/conftest.py
+++ python_modules/dagster-graphql/dagster_graphql_tests/conftest.py
@@ -11,3 +11,16 @@ def unset_dagster_home():
     yield
     if old_env is not None:
         os.environ["DAGSTER_HOME"] = old_env
+
+
+import pytest
+from dagster._core.definitions.decorators.op_decorator import CODE_ORIGIN_ENABLED
+
+
+@pytest.fixture
+def ignore_code_origin():
+    CODE_ORIGIN_ENABLED[0] = False
+
+    yield
+
+    CODE_ORIGIN_ENABLED[0] = True
