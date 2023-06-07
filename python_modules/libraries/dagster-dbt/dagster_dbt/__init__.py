0c8a1c6fc29953a8fc926dcff3f38943c8f84ac7
Sean Mackesey <s.mackesey@gmail.com>
2023-06-07 10:51:49 -0400

    isort comments

--- python_modules/libraries/dagster-dbt/dagster_dbt/__init__.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt/__init__.py
@@ -41,7 +41,7 @@ from .ops import (
 from .types import DbtOutput as DbtOutput
 from .version import __version__ as __version__
 
-# isort: split
+# ruff: isort: split
 
 # ########################
 # ##### DYNAMIC IMPORTS
